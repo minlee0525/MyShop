80f8d42efed18cfa015e2f15f8246577df7eb67e
robot <minlee0525@naver.com>
2022-05-27 11:01:44 +0900

    20220527_2

---
+++ app/src/main/java/com/example/MyShop/CallActivity.java
@@ -0,0 +1,16 @@
+package com.example.MyShop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.MyShop.R;
+
+public class CallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call);
+    }
+}
