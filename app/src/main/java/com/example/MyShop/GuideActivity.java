a8029fc0be2941f4f009036cebcee435e2cab9d1
robot <minlee0525@naver.com>
2022-05-27 12:08:14 +0900

    20220527_마지막

--- app/src/main/java/com/example/MyShop/GuideActivity.java
+++ app/src/main/java/com/example/MyShop/GuideActivity.java
@@ -2,7 +2,10 @@ package com.example.MyShop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.MyShop.R;
 
@@ -12,5 +15,15 @@ public class GuideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
+
+        findViewById(R.id.img_map).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:37.4807426,127.084375?z=16"));
+
+                startActivity(intent);
+            }
+        });
+
     }
 }
